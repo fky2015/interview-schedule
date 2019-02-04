d71d1dd7163dbc8289586d63a49d908bb02cdf7d
FKYnJYQ <loveress01@gmail.com>
2019-02-04 15:22:26 +0800

    test django rest framework

--- schedule_backend/schedule_backend/settings.py
+++ schedule_backend/schedule_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'timelines',
     'users'
 ]
@@ -124,3 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# reat_framework
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
