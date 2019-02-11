73ae299ba31a371e5d85eb098ef1128a6f919ec3
FKYnJYQ <loveress01@gmail.com>
2019-02-11 14:15:08 +0800

    add allowed host to any

--- schedule_backend/schedule_backend/settings.py
+++ schedule_backend/schedule_backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '&y&q+-uf03y!h8hmbukee4)+qvqclka&es(#$pjp5&ym4oovtw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
