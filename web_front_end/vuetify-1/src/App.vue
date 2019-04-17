d8f19910020bdc4e4a3c3f2328f13eb4a04a5af8
ErvinXie <465265950@qq.com>
2019-04-17 16:07:16 +0800

    basics

---
+++ web_front_end/vuetify-1/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <tool-bar></tool-bar>
+    <router-view/>
+    
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ToolBar from './components/ToolBar'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ToolBar
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+
