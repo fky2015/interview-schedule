797de5dc66be88dea4c928edc26e8b4287aa0cda
ErvinXie <465265950@qq.com>
2019-04-17 17:10:20 +0800

    导入成功！

---
+++ web_front_end/iclub/src/components/ToolBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-toolbar>
+      <v-toolbar-title 
+        
+        class="headline"
+      >
+        <span class="font-weight-bold">i 社团 </span>
+      </v-toolbar-title>
+
+      <v-btn
+        outline
+        href="#/"
+        class="red--text text--lighten--1"
+      >
+      <span>广场</span>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+      <v-btn
+        depressed
+        href="#/sign-up"
+        class="red lighten-1 white--text"
+      >
+        <span>注册</span>
+      </v-btn>
+
+      <v-btn
+        outline
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        class="red--text text--lighten--1"
+      >
+        <span>登陆</span>
+      </v-btn>
+    </v-toolbar>
+</template>
+<script>
+export default {
+  methods:{
+    back_to_home:function(){
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+<style>
+
+</style>
+
---
+++ web_front_end/iclub/src/views/Me.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container class="yellow">
+        <v-layout column align-center>
+            <v-avatar>
+                <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+            </v-avatar>
+        </v-layout>
+    </v-container>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
