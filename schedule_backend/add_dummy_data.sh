77e8c747cf83f56ab83ecbff1e7737a7c600ce19
FKYnJYQ <loveress01@gmail.com>
2019-04-29 20:26:11 +0800

    auto add init data

--- schedule_backend/add_dummy_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat add_dummy_data.py | tr '\n' ';' | sed 's/^/"&/g' | sed 's/$/&"/g' | xargs python manage.py shell -c
-
--- update_deploy.sh
+++ update_deploy.sh
@@ -8,3 +8,5 @@ git pull
 $DOSE down
 $DOSE pull
 $DOSE up -d
+
+$DOSE exec app bash -c 'python manage.py shell < add_dummy_data.py'
